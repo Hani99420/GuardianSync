a2e2c905fa0aaffd433c279fb148e0772b1b9891
Hani99420 <h6231857@gmail.com>
2026-06-13 10:48:31 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,17 @@
+const CACHE_VERSION = 'v2';
+
 self.addEventListener('install', function(e) {
-  console.log('GuardianSync service worker installed');
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(keys => 
+            Promise.all(keys.map(key => caches.delete(key)))
+        )
+    );
 });
 
 self.addEventListener('fetch', function(e) {
-  e.respondWith(fetch(e.request));
+    e.respondWith(fetch(e.request));
 });
